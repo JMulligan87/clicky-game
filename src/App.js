9e47a4fdbe25582383886d634a23c716d42a516e
DobisPR <47193076+JMulligan87@users.noreply.github.com>
2019-10-16 22:06:57 -0400

    updated jumbotron background

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import Jumbotron from "./components/Jumbotron";
-import friends from "./friends.json";
+import characters from "./characters.json";
 import Container from "./components/Container";
 // import ClickGame from "./components/ClickGame";
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
+    characters
   };
 
 
