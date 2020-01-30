0373a1903b160b29eb3cd4d7c3614c01956b3cf1
DobisPR <47193076+JMulligan87@users.noreply.github.com>
2020-01-29 21:03:07 -0500

    updated code so current score increments again after being set to zero

--- src/App.js
+++ src/App.js
@@ -1,86 +1,73 @@
-import React, { Component } from "react";
-import Jumbotron from "./components/Jumbotron";
-import characters from "./characters.json";
-import Container from "./components/Container";
-import ClickGame from "./components/ClickGame"
+import React, { Component } from 'react';
+import Jumbotron from './components/Jumbotron';
+import characters from './characters.json';
+import Container from './components/Container';
+import ClickGame from './components/ClickGame';
 
 class App extends Component {
-  // Setting this.state.friends to the friends json array
   state = {
     characters: [...characters],
     currentScore: 0,
     highScore: 0
   };
 
-
-
-
-  onClick = (id) => {
-    console.log(id)
+  onClick = id => {
+    console.log(id);
     let isRight = false;
+    const characters = this.state.characters;
+
     characters.forEach(character => {
       if (id === character.id) {
         if (!character.clicked) {
-          isRight = true
+          isRight = true;
           character.clicked = true;
         }
-        console.log(isRight)
-        this.shuffleCards();
       }
-    })
+    });
     if (isRight === true) {
       this.rightClick(characters);
     } else {
       this.wrongClick(characters);
     }
+  };
 
-  }
+  // shuffleCards = characters => {
+  //   let shuffle = this.state.characters.slice();
 
-  shuffleCards = (characters) => {
-    let shuffle = this.state.characters.slice();
+  //   for (let i = shuffle.length - 1; i > 0; i--) {
+  //     let j = Math.floor(Math.random() * (i + 1));
+  //     [shuffle[i], shuffle[j]] = [shuffle[j], shuffle[i]];
+  //   }
+  //   this.setState({ characters: shuffle });
+  // };
 
-    for (let i = shuffle.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      [shuffle[i], shuffle[j]] = [shuffle[j], shuffle[i]];
-    }
-    this.setState({ characters: shuffle });
-  }
-
-  rightClick = (characters) => {
-    let test = this.state.currentScore + 1;
+  rightClick = characters => {
+    const shuffle = characters.sort(() => 0.5 - Math.random());
+    let currentScore = this.state.currentScore + 1;
     let highScore = this.state.highScore;
 
-    if (test > highScore) {
-      this.setState({
-        currentScore: test,
-        highScore: test
-      })
-
+    if (currentScore > highScore) {
+      highScore = currentScore;
     }
-  }
+    this.setState({
+      characters: shuffle,
+      currentScore: currentScore,
+      highScore: highScore
+    });
+  };
 
-  wrongClick = (characters) => {
+  wrongClick = characters => {
+    const shuffle = characters.sort(() => 0.5 - Math.random());
 
-    this.setState(characters => ({
-      currentScore: 0
-    }));
-    console.log(characters)
-  }
-  // if(characters.clicked===true){
-  //   // console.log(this)
-  //   this.setState({clicked: false,
-  //     currentScore: 0
-  //   })
-  // }else{
-  //   this.setState({clicked: true})
-  //   this.setState({currentScore: test})
-  // }
+    shuffle.forEach(character => (character.clicked = false));
 
+    this.setState({
+      currentScore: 0,
+      characters: shuffle
+    });
+    alert('Game Over! Please Play again.');
+  };
 
-
-
-
-  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Container>
@@ -88,7 +75,7 @@ class App extends Component {
           currentScore={this.state.currentScore}
           highScore={this.state.highScore}
         />
-        <div className="row">
+        <div className='row'>
           {this.state.characters.map(character => (
             <ClickGame
               image={character.image}
